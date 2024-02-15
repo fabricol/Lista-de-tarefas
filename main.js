f6118a2a25fabd0dba0dc780dd70fbceca501918
fabricol <fabricioctnmiranda@gmail.com>
2024-02-15 10:54:33 -0300

    finalização do projeto

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    });
+
+    $('#botaoReset').click(function() {
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const descricaoTarefa = $('#endereco-imagem-nova').val();
+        const novoItem = $(`<li><input type="checkbox" class="checkbox-tarefa"><span class="tarefa">${descricaoTarefa}</span></li>`);
+        novoItem.appendTo('ul');
+        novoItem.fadeIn(500);
+        $('#endereco-imagem-nova').val('');
+    });
+
+    $('ul').on('change', '.checkbox-tarefa', function() {
+        const li = $(this).closest('li');
+        const span = li.find('.tarefa');
+        span.toggleClass('checked');
+    });
+});
